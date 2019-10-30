40eb002ff4d2fa495986074b527a3c13837e90dc
Michele Gruppioni <gruppiofigo@hotmail.it>
2019-10-30 16:22:04 +0100

    Added bounce lib but it seems to not work

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,43 +1,31 @@
 #include <Arduino.h>
-#include <Keyboard.h>
+#include <Bounce2.h>
+
+#define START_PIN 36
+#define NUMBER_OF_PINS 18
+#define DEBOUNCE_TIME 3
+
+char startCharacters[] = { 'Q', 'W', 'E', 'R', 'T', 'Y', 'A', 'S', 'D', 'F', 'G', 'H', 'Z', 'X', 'C', 'V', 'B', 'N'};
+char stopCharacters[] =  { 'q', 'w', 'e', 'r', 't', 'y', 'a', 's', 'd', 'f', 'g', 'h', 'z', 'x', 'c', 'v', 'b', 'n'};
+Bounce * buttons = new Bounce[NUMBER_OF_PINS];
 
 void setup() {
-  Keyboard.begin();
+  for(int i = 0; i < NUMBER_OF_PINS; i++) {
+    buttons[i].attach(i + START_PIN, INPUT_PULLUP);
+    buttons[i].interval(DEBOUNCE_TIME);
+  }
+
+  //pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  Keyboard.press('a');
-  delay(100);
-  Keyboard.release('a');
+  for(int i = 0; i < NUMBER_OF_PINS; i++) {
+    buttons[i].update();
+    if(buttons[i].fell()) {
+      Serial.print(startCharacters[i]);
+    } else if(buttons[i].rose()) {
+      Serial.print(stopCharacters[i]);
+    }
+  }
 }
-
-// #define START_PIN 36
-// #define NUMBER_OF_PINS 18
-// #define DEBOUNCE_TIME 100
-
-// char characters[] = { 'q', 'w', 'e', 'r', 't', 'y', 'a', 's', 'd', 'f', 'g', 'h', 'z', 'x', 'c', 'v', 'b', 'n'};
-// unsigned long lastPressTime[NUMBER_OF_PINS];
-
-// void setup() {
-//   for(int i = 0; i < NUMBER_OF_PINS; i++) {
-//     pinMode(i + START_PIN, INPUT_PULLUP);
-//   }
-
-//   memset(lastPressTime, 0, sizeof(lastPressTime));
-
-//   //pinMode(LED_BUILTIN, OUTPUT);
-//   Serial.begin(9600);
-// }
-
-// void loop() {
-//   for(int i = 0; i < NUMBER_OF_PINS; i++) {
-//     bool isPressed = (digitalRead(i + START_PIN) == LOW);
-// 	if (isPressed) {
-// 		unsigned long t = millis();
-// 		if(t - lastPressTime[i] > DEBOUNCE_TIME) {
-// 			Serial.print(characters[i]);
-// 		}
-// 		lastPressTime[i] = t;
-// 	}
-//   }
-// }
