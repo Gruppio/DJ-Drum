b1f0ca2f46d8cf47185242e21540b106f94fc2fa
Michele Gruppioni <gruppiofigo@hotmail.it>
2019-11-10 12:47:12 +0100

    Add MIDI communication

--- Arduino/ArduinoDue_SAMD/DJDrums/DJDrums.ino
+++ Arduino/ArduinoDue_SAMD/DJDrums/DJDrums.ino
@@ -1,23 +1,81 @@
 #include <Arduino.h>
-#include <Keyboard.h>
+//#include <Keyboard.h>
+#include <MIDIUSB.h>
 
 // In order to flash the arduino Due(milanove) you have to download the SAMD board in the board manager
 // keep press the erase button, then press the reset button and then you can program it
 
+#define velocity 64
+#define channel 0
+#define octave 4
+
 #define START_PIN 36
 #define NUMBER_OF_PINS 18
 #define DEBOUNCE_TIME 50
+
+// Keyboard
+// B:    | 2 | 4 |   | 7 | 9 | 11 |     | 14 | 16 |
+// W:  | 1 | 3 | 5 | 6 | 8 | 10 | 12 | 13 | 15 | 17 | 18 |
+
+// Drum
+//   1     2     3     4     5     6
+// ( 2 ) ( 4 ) ( 7 ) ( 9 ) ( 11 )( 14 )
+
+//   7     8     9    10     11    12
+// ( 1 ) ( 3 ) ( 5 ) ( 6 ) ( 8 ) ( 10 )
+
+//   13    14    15    16    17    18
+// ( 12 )( 13 )( 15 )( 17 )( 18 )( 16 )
+
 
 // char startCharacters[] = {'Q', 'W', 'E', 'R', 'T', 'Y', 'A', 'S', 'D', 'F', 'G', 'H', 'Z', 'X', 'C', 'V', 'B', 'N'};
 // char stopCharacters[] = {'q', 'w', 'e', 'r', 't', 'y', 'a', 's', 'd', 'f', 'g', 'h', 'z', 'x', 'c', 'v', 'b', 'n'};
 char keyboardCharacters[] = { 'w', 'e', 't', 'y', 'u', 'o', 
                               'a', 's', 'd', 'f', 'g', 'h', 
                               'j', 'k', 'l', ';', '\'', 'p'};
+
+byte midiNoteOffset[] = { 1,  3,  6,  8,  10, 13, 
+                          0,  2,  4,  5,  7,  9, 
+                          11, 12, 14, 16, 17, 15 };
+                                                      
 unsigned long lastChangedTime[NUMBER_OF_PINS];
 uint8_t previousPressedState[NUMBER_OF_PINS];
 
+// First parameter is the event type (0x09 = note on, 0x08 = note off).
+// Second parameter is note-on/note-off, combined with the channel.
+// Channel can be anything between 0-15. Typically reported to the user as 1-16.
+// Third parameter is the note number (48 = middle C).
+// Fourth parameter is the velocity (64 = normal, 127 = fastest).
+
+byte noteForPad(int pad) {
+  byte offset = midiNoteOffset[pad];
+  byte note = (octave + 1) * 12 + offset;
+  return note;
+}
+
+void noteOn(int pad) {
+  byte note = noteForPad(pad);
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, note, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(int pad) {
+  byte note = noteForPad(pad);
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, note, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
+
 void setup()
 {
+  // Setup all pins (digital and analog) in INPUT mode (default is nothing)
+  /*for (uint32_t ul = 0 ; ul < NUM_DIGITAL_PINS ; ul++ )
+  {
+    pinMode( ul, INPUT ) ;
+  }*/
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  
   for (int i = 0; i < NUMBER_OF_PINS; i++)
   {
     pinMode(i + START_PIN, INPUT_PULLUP);
@@ -28,7 +86,7 @@ void setup()
 
   //pinMode(LED_BUILTIN, OUTPUT);
   //Serial.begin(9600);
-  Keyboard.begin();
+  //Keyboard.begin();
 }
 
 void loop()
@@ -44,13 +102,16 @@ void loop()
         continue;
 
       if(isPressedState) {
-        Keyboard.press(keyboardCharacters[i]);
+        noteOn(i);
+        //Keyboard.press(keyboardCharacters[i]);
       } else {
-        Keyboard.release(keyboardCharacters[i]);
+        noteOff(i);
+        //Keyboard.release(keyboardCharacters[i]);
       }
       //Serial.print(isPressedState ? startCharacters[i] : stopCharacters[i]);
       lastChangedTime[i] = t;
       previousPressedState[i] = isPressedState;
     }
   }
+  MidiUSB.flush();
 }
