8a8db770521b32b2521d4a05307c2d12ced1be1f
Michele Gruppioni <gruppiofigo@hotmail.it>
2019-12-08 20:04:33 +0100

    WIP

--- Arduino/ArduinoDue_SAMD/DJDrums/DJDrums.ino
+++ Arduino/ArduinoDue_SAMD/DJDrums/DJDrums.ino
@@ -1,10 +1,11 @@
 #include <Throttle.h>
 #include <Arduino.h>
 #include <MIDIUSB.h>
+#include <TM1637Display.h>
 
 // In order to flash the arduino Due(milanove) you have to download the SAMD ( search for "DUE" ) board in the board manager
 // keep press the erase button, then press the reset button and then you can program it
-// You need to install also MIDIUSB lib
+// You need to install also MIDIUSB, Throttle (Mine), TM1637Display (https://github.com/avishorp/TM1637) libs
 
 #define velocity 64
 
@@ -18,7 +19,11 @@
 #define NUMBER_OF_MODALITIES 21
 #define DEFAULT_OCTAVE 4
 
-#define DEBUG 1
+#define DEBUG 0
+#define SERIAL_ENABLED 0
+
+#define DIO 2 // Pink wire
+#define CLK 3 // Gray wire
 
 // Keyboard
 // Black Keys:    | 2 | 4 |   | 7 | 9 | 11 |     | 14 | 16 |
@@ -26,7 +31,7 @@
 
 // Drum with No Intonation 
 // Pad Number
-// (Offser)
+// (Offset)
 //   1     2     3     4     5     6
 // ( 2 ) ( 4 ) ( 7 ) ( 9 ) ( 11 )( 14 )
 
@@ -39,7 +44,7 @@
 
 // Drum with Intonation
 // Pad Number
-// (Offser)
+// (Offset)
 //     1      2      3      4      5      6
 // ( -10 ) ( -8 ) ( -7 ) ( -5 ) ( -3 ) ( -1 )
 
@@ -56,6 +61,8 @@
 //                               'a', 's', 'd', 'f', 'g', 'h', 
 //                               'j', 'k', 'l', ';', '\'', 'p'};
 
+TM1637Display display(CLK, DIO);
+
 int padOffsetsWithNoIntonation[] = {1, 3, 6, 8, 10, 13,
                                     0, 2, 4, 5, 7, 9,
                                     11, 12, 14, 16, 17, 15};
@@ -65,12 +72,12 @@ int padOffsetsWithIntonation[] = {-10, -8, -7, -5, -3, -1,
                                   12, 13, 15, 17, 18, 20};
 
 Throttle pads[NUMBER_OF_PADS];
-Throttle increaseModalityButton(13, INPUT_PULLUP, 100);
-Throttle decreaseModalityButton(12, INPUT_PULLUP, 100);
-Throttle increaseOctaveButton(11, INPUT_PULLUP, 100);
-Throttle decreaseOctaveButton(10, INPUT_PULLUP, 100);
-Throttle increaseIntonationButton(9, INPUT_PULLUP, 100);
-Throttle decreaseIntonationButton(8, INPUT_PULLUP, 100);
+Throttle increaseModalityButton(22, INPUT_PULLUP, 100);
+Throttle decreaseModalityButton(23, INPUT_PULLUP, 100);
+Throttle increaseOctaveButton(24, INPUT_PULLUP, 100);
+Throttle decreaseOctaveButton(25, INPUT_PULLUP, 100);
+Throttle increaseIntonationButton(26, INPUT_PULLUP, 100);
+Throttle decreaseIntonationButton(27, INPUT_PULLUP, 100);
 
 
 int modality = 11;
@@ -79,31 +86,37 @@ int intonationForModality[NUMBER_OF_MODALITIES];
 
 void increaseModality() {
   modality = constrain(modality + 1, 0, NUMBER_OF_MODALITIES);
+  updateDisplay();
   printState();
 }
 
 void decreaseModality() {
   modality = constrain(modality - 1, 0, NUMBER_OF_MODALITIES);
+  updateDisplay();
   printState();
 }
 
 void increaseOctave() {
   octaveForModality[modality] = constrain(octaveForModality[modality] + 1, 0, 7);
+  updateDisplay();
   printState();
 }
 
 void decreaseOctave() {
   octaveForModality[modality] = constrain(octaveForModality[modality] - 1, 0, 7);
+  updateDisplay();
   printState();
 }
 
 void increaseIntonation() {
   intonationForModality[modality] = constrain(intonationForModality[modality] + 1, 0, 11);
+  updateDisplay();
   printState();
 }
 
 void decreaseIntonation() {
   intonationForModality[modality] = constrain(intonationForModality[modality] - 1, 0, 11);
+  updateDisplay();
   printState();
 }
 
@@ -123,7 +136,7 @@ byte getChannel(int pad) {
 
   if (shouldUseIntonation()) {
     // Modality from 11 - 20
-    return (byte)(modality - 10);
+    return (byte)(modality - 11);
   } else {
     // Modality from 0 - 9
     return (byte)(9 - modality);
@@ -157,31 +170,6 @@ byte noteForPad(int pad) {
   return (byte)constrainedNote;
 }
 
-void printState() {
-  if (DEBUG) {
-    Serial.println("");
-    Serial.println("Current State:");
-    Serial.print("Modality: "); Serial.println(modality);
-    Serial.print("Octave: "); Serial.println(octaveForModality[modality]);
-    Serial.print("Intonation: "); Serial.println(intonationForModality[modality]);
-    Serial.println("");
-  }
-}
-
-void printNoteOn(byte note) {
-  if (DEBUG) {
-    Serial.println("");
-    Serial.print("Note On: "); Serial.println(note);
-  }
-}
-
-void printNoteOff(byte note) {
-  if (DEBUG) {
-    Serial.println("");
-    Serial.print("Note Off: "); Serial.println(note);
-  }
-}
-
 // First parameter is the event type (0x09 = note on, 0x08 = note off).
 // Second parameter is note-on/note-off, combined with the channel.
 // Channel can be anything between 0-15. Typically reported to the user as 1-16.
@@ -203,6 +191,130 @@ void noteOff(int pad) {
   printNoteOff(note);
 }
 
+//
+//      A
+//     ---
+//  F |   | B
+//     -G-
+//  E |   | C
+//     ---
+//      D
+byte digitFor(char character) {
+  switch (character) {
+    //                 XGFEDCBA
+    case '0': return 0b00111111;    // 0
+    case '1': return 0b00000110;    // 1
+    case '2': return 0b01011011;    // 2
+    case '3': return 0b01001111;    // 3
+    case '4': return 0b01100110;    // 4
+    case '5': return 0b01101101;    // 5
+    case '6': return 0b01111101;    // 6
+    case '7': return 0b00000111;    // 7
+    case '8': return 0b01111111;    // 8
+    case '9': return 0b01101111;    // 9
+    case 'a': return 0b01110111;    // A
+    case 'b': return 0b01111100;    // B
+    case 'c': return 0b00111001;    // C
+    case 'd': return 0b01011110;    // D
+    case 'e': return 0b01111001;    // E
+    case 'f': return 0b01110001;    // F
+    case 'g': return 0b01111101;    // G
+    case 'h': return 0b01110110;    // H
+    case 'k': return 0b01110110;    // K
+    case 'i': return 0b00000110;    // I
+    case 'l': return 0b00110100;    // L
+    case 'm': return 0b00110111;    // M
+    case 'n': return 0b01010100;    // N
+    case 'o': return 0b00111111;    // O
+    case 'p': return 0b01110011;    // P
+    case 'q': return 0b01011100;    // Q
+    case 'r': return 0b01010000;    // R
+    case 's': return 0b01101101;    // S
+    case 't': return 0b01111000;    // T
+    case 'u': return 0b00111110;    // U
+    case 'v': return 0b00011100;    // V
+    case 'w': return 0b00011100;    // W
+    case 'x': return 0b01110110;    // X
+    case 'y': return 0b01100110;    // Y
+    case 'z': return 0b01011011;    // Z
+    case '.': return 0b10000000;    // .
+    case '=': return 0b01001001;    // =
+  }
+  return 0b00000000;
+}
+
+byte digitFor(char character, bool point) {
+  byte value = digitFor(character);
+  return point ? (0b10000000 | value) : value;
+}
+
+byte getModalityDigit() {
+  if (is3ChannelModality())
+    return digitFor('=', true);
+
+  char channel = '0' + getChannel(0);
+  return digitFor(channel, shouldUseIntonation());
+}
+
+byte getOctaveDigit() {
+  char octave = '0' + (char)octaveForModality[modality];
+  return digitFor(octave);
+}
+
+byte getIntonationDigits(bool first) {
+  byte intonation = getIntonation();
+  switch (intonation)
+  {
+  case 0:
+    return first ? digitFor('d') : digitFor('o');
+
+  case 1:
+    return first ? digitFor('d') : digitFor('h');
+
+  case 2:
+    return first ? digitFor('r') : digitFor('e');
+
+  case 3:
+    return first ? digitFor('r') : digitFor('h');
+
+  case 4:
+    return first ? digitFor('m') : digitFor('i');
+  
+  case 5:
+    return first ? digitFor('f') : digitFor('a');
+
+  case 6:
+    return first ? digitFor('f') : digitFor('H');
+
+  case 7:
+    return first ? digitFor('s') : digitFor('o');
+
+  case 8:
+    return first ? digitFor('s') : digitFor('h');
+
+  case 9:
+    return first ? digitFor('l') : digitFor('a');
+
+  case 10:
+    return first ? digitFor('l') : digitFor('h');
+
+  case 11:
+    return first ? digitFor('s') : digitFor('i');
+  
+  default:
+    return digitFor('0');
+  }
+}
+
+void updateDisplay() {
+  byte digits[4];
+  digits[0] = getModalityDigit();
+  digits[1] = getOctaveDigit();
+  digits[2] = getIntonationDigits(true);
+  digits[3] = getIntonationDigits(false);
+  display.setSegments(digits, 4, 0);
+}
+
 void setup()
 {
   // Is in Pin 13 that is already used
@@ -221,18 +333,19 @@ void setup()
     octaveForModality[i] = DEFAULT_OCTAVE;
   }
 
-  
-  pinMode(13, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
-  pinMode(11, INPUT_PULLUP);
-  pinMode(10, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
+  increaseModalityButton.attach(22, INPUT_PULLUP);
+  decreaseModalityButton.attach(23, INPUT_PULLUP);
+  increaseOctaveButton.attach(24, INPUT_PULLUP);
+  decreaseOctaveButton.attach(25, INPUT_PULLUP);
+  increaseIntonationButton.attach(26, INPUT_PULLUP);
+  decreaseIntonationButton.attach(27, INPUT_PULLUP);
 
   Serial.begin(9600);
 
   delay(1000);
   printState();
+  display.setBrightness(0x0a);
+  updateDisplay();
 }
 
 void loop()
@@ -258,14 +371,43 @@ void loop()
     if (pads[i].fell())
     {
       noteOn(i);
-      Serial.write(getNoteOnSerialByte(i));
+      if (SERIAL_ENABLED) {
+        Serial.write(getNoteOnSerialByte(i));
+      }
     }
 
     if (pads[i].rose())
     {
       noteOff(i);
-      Serial.write(getNoteOffSerialByte(i));
+      if (SERIAL_ENABLED) {
+        Serial.write(getNoteOffSerialByte(i));
+      }
     }
   }
   MidiUSB.flush();
+}
+
+void printState() {
+  if (DEBUG) {
+    Serial.println("");
+    Serial.println("Current State:");
+    Serial.print("Modality: "); Serial.println(modality);
+    Serial.print("Octave: "); Serial.println(octaveForModality[modality]);
+    Serial.print("Intonation: "); Serial.println(intonationForModality[modality]);
+    Serial.println("");
+  }
+}
+
+void printNoteOn(byte note) {
+  if (DEBUG) {
+    Serial.println("");
+    Serial.print("Note On: "); Serial.println(note);
+  }
+}
+
+void printNoteOff(byte note) {
+  if (DEBUG) {
+    Serial.println("");
+    Serial.print("Note Off: "); Serial.println(note);
+  }
 }
